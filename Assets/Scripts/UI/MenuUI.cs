b792cf549593c742b052166c4cff1f48bf7a91f8
Justyna Dąbrowska <tyska.dabrowska@gmail.com>
2024-12-03 03:35:32 +0100

    added quit button, not fully safe but works

---
+++ Assets/Scripts/UI/MenuUI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUI : MonoBehaviour
+{
+    [SerializeField] public Button QuitGameButton;
+
+    private void Awake()
+    {
+        QuitGameButton.onClick.AddListener(async () =>
+        {
+            RelayManager.Instance.LeaveRelay();
+            AuthenticationService.Instance.SignOut();
+            ScenesManager.Instance.LoadScene(ScenesManager.Scene.StartWindow);
+        });
+    }
+}
